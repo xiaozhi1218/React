e4df947ec2c47bf36be9da50dbfa1689e0f0780b
chenyongzhi <chenyongzhijjk@163.com>
2021-02-21 22:04:42 +0800

    package

--- 04_learn_component/src/index.js
+++ 04_learn_component/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import App from './01_组件的定义方式/01_组件的定义';
+// import App from './01_组件的定义方式/02_render返回值';
+// import App from './02_组件的生命周期/App';
+// import App from './03_组件间的通信/01_组件间的嵌套';
+// import App from './03_组件间的通信/02_父传子通信-类组件';
+// import App from './03_组件间的通信/03_父传子通信-函数组件';
+// import App from './03_组件间的通信/04_父传子通信-属性验证';
+// import App from './03_组件间的通信/05_子传父通信-函数传递';
+
 // import App from './04_组件通信案例/App';
 // import './04_组件通信案例/style.css';
 
@@ -30,7 +39,7 @@ import ReactDOM from 'react-dom';
 // import App from './10_受控和非受控组件/01_refs的使用';
 // import App from './10_受控和非受控组件/02_受控组件的基本使用';
 // import App from './10_受控和非受控组件/03_受控组件-select的使用';
-// import App from './10_受控和非受控组件/04_受控组件-多输入';
+import App from './10_受控和非受控组件/04_受控组件-多输入';
 // import App from './10_受控和非受控组件/05_非受控组件的使用';
 
 // import App from './11_高阶组件的使用/01_高阶组件的定义方式';
@@ -44,6 +53,6 @@ import ReactDOM from 'react-dom';
 // import App from './12_组件内容补充/02_portals的使用';
 // import "./css/index.css";
 // import App from './12_组件内容补充/03_fragments的使用';
-import App from './12_组件内容补充/04_strictMode的使用';
+// import App from './12_组件内容补充/04_strictMode的使用';
 
 ReactDOM.render(<App name="why"/>, document.getElementById('root'));
