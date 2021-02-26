5a2f05298de28e2d36b8377dd49dcbbe36adee23
chenyongzhi <chenyongzhijjk@163.com>
2021-02-26 17:06:06 +0800

    styled-components

--- 05_react_style/src/index.js
+++ 05_react_style/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import App from './01_内联样式/App';
+// import App from './02_普通的css/app';
 // import App from './03_css_modules/app';
 import App from './04_styled-components/app';
 
