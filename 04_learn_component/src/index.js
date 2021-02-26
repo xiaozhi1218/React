c5693de1fa05d79e77b8279910093beb4d5d24ac
chenyongzhi <chenyongzhijjk@163.com>
2021-02-26 14:51:01 +0800

    react style

--- 04_learn_component/src/index.js
+++ 04_learn_component/src/index.js
@@ -39,7 +39,7 @@ import ReactDOM from 'react-dom';
 // import App from './10_受控和非受控组件/01_refs的使用';
 // import App from './10_受控和非受控组件/02_受控组件的基本使用';
 // import App from './10_受控和非受控组件/03_受控组件-select的使用';
-import App from './10_受控和非受控组件/04_受控组件-多输入';
+// import App from './10_受控和非受控组件/04_受控组件-多输入';
 // import App from './10_受控和非受控组件/05_非受控组件的使用';
 
 // import App from './11_高阶组件的使用/01_高阶组件的定义方式';
@@ -53,6 +53,6 @@ import App from './10_受控和非受控组件/04_受控组件-多输入';
 // import App from './12_组件内容补充/02_portals的使用';
 // import "./css/index.css";
 // import App from './12_组件内容补充/03_fragments的使用';
-// import App from './12_组件内容补充/04_strictMode的使用';
+import App from './12_组件内容补充/04_strictMode的使用';
 
 ReactDOM.render(<App name="why"/>, document.getElementById('root'));
