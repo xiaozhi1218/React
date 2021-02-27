dbd4e98ec9e18199fac2f53e9bf4347d39a17a4d
chenyongzhi <chenyongzhijjk@163.com>
2021-02-27 13:25:25 +0800

    axios

--- 07_learn_axios/src/index.js
+++ 07_learn_axios/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import axios from 'axios';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// 7.默认配置
+axios.defaults.baseURL = "https://httpbin.org";
+axios.defaults.timeout = 5000;
+axios.defaults.headers.common["token"] = "dafdafadfadfadfas";
+// axios.defaults.headers.post["Content-Type"] = "application/text";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +17,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
