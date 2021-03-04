896882cc3d9c6ee87b334c64157e293cded15e82
chenyongzhi <chenyongzhijjk@163.com>
2021-03-04 22:02:40 +0800

    header footer

--- cy-web-music/src/components/app-header/index.js
+++ cy-web-music/src/components/app-header/index.js
@@ -1,13 +1,57 @@
-import React, { memo } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { memo } from 'react';
 
-export default memo(function HYAppFooter() {
+import { headerLinks } from "@/common/local-data";
+
+import { NavLink } from 'react-router-dom';
+import { SearchOutlined } from '@ant-design/icons'
+import { Input } from "antd";
+import {
+  HeaderWrapper,
+  HeaderLeft,
+  HeaderRight
+} from './style';
+
+export default memo(function HYAppHeader() {
+
+  // 页面代码
+  const showSelectItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink to={item.link}>
+          {item.title}
+          <i className="sprite_01 icon"></i>
+        </NavLink>
+      )
+    } else {
+      return <a href={item.link}>{item.title}</a>
+    }
+  }
+
+  // 返回的jsx
   return (
-    <div>
-      <NavLink to="/">发现音乐</NavLink>
-      <NavLink to="/mine">我的音乐</NavLink>
-      <NavLink to="/friend">我的朋友</NavLink>
-    </div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a href="#/" className="logo sprite_01">网易云音乐</a>
+          <div className="select-list">
+            {
+              headerLinks.map((item, index) => {
+                return (
+                  <div key={item.title} className="select-item">
+                    {showSelectItem(item, index)}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />}/>
+          <div className="center">创作者中心</div>
+          <div>登录</div>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
   )
 })
-
