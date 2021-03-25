5103ffac52842010afca5dad1d02ac53008b1c11
chenyongzhi <chenyongzhijjk@163.com>
2021-03-25 16:04:33 +0800

    局部样式和全局样式

--- 05_react_style/src/03_css_modules/app/index.js
+++ 05_react_style/src/03_css_modules/app/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 
-import appStyle from './style.module.css';
+import './common.css'
+import style from './style.module.css';
 
 import Home from '../home';
 import Profile from '../profile';
@@ -10,7 +11,8 @@ export default class App extends PureComponent {
     return (
       <div id="app">
         App
-        <h2 className={appStyle.title}>我是App的title</h2>
+        <h2 className={'fs ' + style['app-title']}>我是App的title1</h2>
+        <h2 className={`fs ${style['app-title']}`}>我是App的title2</h2>
         <Home/>
         <Profile/>
       </div>
